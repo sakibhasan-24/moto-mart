22127d0952ef84dd5cadf26a01c8f4e77e25bc07
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-06-29 15:33:43 +0600

    changes

--- src/redux/api/baseApi.tsx
+++ src/redux/api/baseApi.tsx
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://bike-shop-server-weld.vercel.app/api/products",
+    baseUrl: "https://moto-gamma.vercel.app/api/",
     credentials: "include",
   }),
   endpoints: (builder) => ({}),
