a4b285a7ed998fbb92839e378c7f4af708eb98d1
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-06-29 15:13:36 +0600

    push link

--- src/redux/api/baseApi.tsx
+++ src/redux/api/baseApi.tsx
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/",
+    baseUrl: "https://moto-gamma.vercel.apapi/",
     credentials: "include",
   }),
   endpoints: (builder) => ({}),
